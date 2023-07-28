a17682de9b2dbcc38659ae5ad1be4450c44f433b
Jezebel1990 <jezebelguedes@gmail.com>
2023-07-28 17:59:31 -0300

    create routes

---
+++ backend/src/routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const multer = require('multer');
+
+
+const router = express.Router();// Create the router instance
+
+
+//Multer configuration for file upload
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, 'uploads/'); // Create a folder called 'uploads' to save the files
+    },
+     filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.sales)
+     }
+    });
+    
+    
+    const upload = multer({ storage: storage });
+
+    router.post('/upload', upload.single('file'), (req, res) => {
+
+    res.json({ message: 'Upload realizado com sucesso!'});
+
+    });
+
+    module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,37 @@
-import express from 'express';
+const express = require('express');
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
 
 const app = express()
+const port = 3000;
 
-app.use(express.json());
+//Import the routes
+const routes = require('./routes');
+
+dotenv.config();
+
+const DB_USER = process.env.DB_USER;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_PORT = process.env.DB_PORT;
 
 
-app.listen(3000);
+const pool = new Pool({
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+});
+
+// Add the pool to the app locals, so it's accessible in the routes
+app.locals.pool = pool;
+
+app.use('/', routes);
+
+app.listen(port, () => {
+   console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+
