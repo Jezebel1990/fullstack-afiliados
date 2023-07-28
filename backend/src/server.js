11aefda9aef8445e6d5a0a36c2eacedceec0e579
Jezebel1990 <jezebelguedes@gmail.com>
2023-07-28 18:26:53 -0300

    create config  database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,10 @@
 const express = require('express');
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
 
 const app = express()
 const port = 3000;
 
 //Import the routes
 const routes = require('./routes');
-
-dotenv.config();
-
-const DB_USER = process.env.DB_USER;
-const DB_HOST = process.env.DB_HOST;
-const DB_NAME = process.env.DB_NAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_PORT = process.env.DB_PORT;
-
-
-const pool = new Pool({
-    user: DB_USER,
-    host: DB_HOST,
-    database: DB_NAME,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-});
-
-// Add the pool to the app locals, so it's accessible in the routes
-app.locals.pool = pool;
 
 app.use('/', routes);
 
