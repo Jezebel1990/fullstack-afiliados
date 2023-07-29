d30d18dcabf7d1322d3ba79db57dcf8add732c79
Jezebel1990 <jezebelguedes@gmail.com>
2023-07-29 02:51:25 -0300

    create models User

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,6 +1,12 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database'
 
+import User from '../app/models/User';
+
+const models = [
+    User
+]
+
 
 class Database {
     constructor(){
@@ -9,6 +15,7 @@ class Database {
 
     init() {
         this.connection = new Sequelize(databaseConfig)
+        models.map(model => model.init(this.connection))
     }
 }
 
