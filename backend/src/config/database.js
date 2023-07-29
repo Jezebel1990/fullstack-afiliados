62d855217362231a6e5643406c0ac73c6c615980
Jezebel1990 <jezebelguedes@gmail.com>
2023-07-29 01:21:18 -0300

    config docker compose

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,17 +1,16 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
-    dialiect: 'postgres',
- 
-host: process.env.DB_HOST,
-port: process.env.DB_PORT,
-username: process.env.DB_USER,
-password: process.env.DB_PASSWORD,
-database: process.env.DB_NAME,
-seerdStorage: 'sequelize',
-define: {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  seederStorage: 'sequelize',
+  define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
-}
-}
+  },
+};
