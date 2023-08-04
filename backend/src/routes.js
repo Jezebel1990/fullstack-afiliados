7bfacc586c9fc2c01fc4722d7f0193c1d20fa8fe
Jezebel1990 <jezebelguedes@gmail.com>
2023-08-04 00:53:26 -0300

    remove routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-//const multer = require('multer');
+
 
 const routes = Router();// Create the router instance
 
@@ -10,12 +10,11 @@ import authMiddleware from './app/middlewares/authMiddleware';
 
 import InputfileController from './app/controllers/InputfileController';
 
-routes.post('/auth', AuthController.login);
-routes.post('/users', UserController.create);
+routes.post('/auth', AuthController.login); //rota login
+routes.post('/users', UserController.create); // rota register
 
 // Rotas protegidas pelo middleware de autenticação
 routes.use(authMiddleware);
-
 routes.post('/protected-route', (req, res) => {
     try {
       // Obtém o id do usuário autenticado a partir do middleware de autenticação
@@ -30,7 +29,7 @@ routes.post('/protected-route', (req, res) => {
 
 
 
-routes.get('/users', UserController.index);
+routes.get('/users', UserController.index); //ver usarios
 routes.get('/users/:id', UserController.show);
 routes.put('/users', UserController.update);
 
@@ -40,25 +39,5 @@ routes.get('/inputfiles', InputfileController.getAll);
 routes.get('/inputfiles/:id', InputfileController.getById);
 routes.put('/inputfiles/:id', InputfileController.update);
 routes.delete('/inputfiles/:id', InputfileController.delete);
-
-
-//Multer configuration for file upload
-//const storage = multer.diskStorage({
-    //destination: function(req, file, cb){
-       // cb(null, 'uploads/'); // Create a folder called 'uploads' to save the files
-   // },
-    // filename: function (req, file, cb) {
-    //    cb(null, Date.now() + '-' + file.sales)
-   //  }
-   // });
-    
-    
-   // const upload = multer({ storage: storage });
-
-    //router.post('/upload', upload.single('file'), (req, res) => {
-
-   // res.json({ message: 'Upload realizado com sucesso!'});
-
-   // });
 
     export default routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,20 @@
 import 'dotenv/config';
-
+import cors from 'cors';
 import express from 'express';
 import routes from './routes';
 
 import './database';
 
-const app = express()
+const app = express();
+app.use(cors({
+   origin: ["http://localhost:3000"],
+   methods: ["POST", "GET", "PUT", "DELETE"],
+   credentials: true
+ }))
+ app.use(express.json());
+ app.use(express.static('public'));
+
 const port = 3000;
-
-app.use(express.json());
-
 app.use(routes);
 
 app.listen(port, () => {
